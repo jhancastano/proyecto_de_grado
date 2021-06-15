868632a1bffd7a62c66e6fe8bc66d98d04a9f7c0
jhancastano <jhancastano@utp.edu.co>
2021-06-15 12:49:27 -0500

    add headers ws

--- liderUTP/src/app/services/user-service.service.ts
+++ liderUTP/src/app/services/user-service.service.ts
@@ -1,19 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class UserServiceService {
   url = environment.baseUrl;
-  constructor(private http: HttpClient, ) {    
-   }
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept-Encoding": "gzip, deflate, br",
+     // Authorization: "my-auth-token",
+      Host: "plataforma.utp.edu.co",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "cross-site",
+    }),
+  };
+  constructor(private http: HttpClient) {}
 
-   login(credentials): Observable<any> {
-    return this.http.post<any>( this.url+'login/token.php', credentials);
+  login(credentials): Observable<any> {
+    console.log()
+    return this.http.post<any>(
+      this.url + `login/token.php?username=${credentials.username}&password=${credentials.password}&service=moodle_mobile_app`,
+      credentials,
+      this.httpOptions
+    );
   }
-
 }
